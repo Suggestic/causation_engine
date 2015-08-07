5571cc239e0d7ab9ed369e1c127c37eea776de37
Ricardo Corral C <ricardo@suggestic.com>
2015-08-07 18:04:23 -0500

    DIAB-324 add parallelized kagglePredict.py

--- kagglePredict.py
+++ kagglePredict.py
@@ -1,8 +1,26 @@
 import os
 from causation import *
 import sys
+from multiprocessing import Pool
+from threading import BoundedSemaphore
+from random import randint
 
-def make_preds(fn_train='CEfinal_valid_pairs.csv'):
+
+dictres = dict()
+
+pool_sema = BoundedSemaphore(value=1)
+
+def ANM_wrap(D):
+    name,X, Y = D[0], D[1], D[2]
+    print 'Processing:', name, len(X), len(Y)
+    res = AMM_bagging_causality_prediction(X,Y)['finalscore']
+    pool_sema.acquire()
+    dictres[name] = res
+    pool_sema.release()
+    print len(dictres)
+    print "%s,%s" % (name,res)
+
+def make_datasets(fn_train='CEfinal_valid_pairs.csv'):
     fin = open(fn_train,'r')
     fin.readline()
     names, Xs, Ys = [], [], []
@@ -15,13 +33,26 @@ def make_preds(fn_train='CEfinal_valid_pairs.csv'):
         Xs.append(X)
         Ys.append(Y)
 
+    return {'names':names,'Xs':Xs,'Ys':Ys}
+
+def main():
+    dss = make_datasets()
+    names, Xs, Ys = dss['names'], dss['Xs'], dss['Ys']
     fout = open('kagglePreds.csv','w',0)
     fout.write("SampleID,Target\n")
+
+    args = [x for x in zip(names,Xs,Ys)]
+
+    p = Pool(randint(2,4))
+    p.map( ANM_wrap, args )
+    p.close()
+    p.join()
+
+
     for i in xrange(len(Xs)):
-        pred  = AMM_bagging_causality_prediction(Xs[i],Ys[i])['finalscore']
-        fout.write("%s,%s\n" % (names[i],pred))
-        print "%s,%s" % (names[i],pred)
+        fout.write("%s,%s\n" % (names[i],dictres[names[i]]))
+
 
 
 if __name__ == '__main__':
-    make_preds()
+    main()
